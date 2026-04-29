29503491245494af4806616f23a27f20d82f9079
Appli EPS CM <lapoulette_64@hotmail.fr>
2026-04-29 19:24:03 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'climbmaster-v5';
+const CACHE_NAME = 'climbmaster-cache-v5';
 const ASSETS = [
   './index.html',
   './manifest.json',
@@ -7,25 +7,10 @@ const ASSETS = [
   'https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.23/jspdf.plugin.autotable.min.js'
 ];
 
-// Installation : Mise en cache
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
-// Activation : Nettoyage
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)));
-    })
-  );
-});
-
-// Fetch : Répondre depuis le cache, sinon réseau
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
